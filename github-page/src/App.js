0a31bf8501a9e851bb96b61a796aa5a617210483
fightforhash <rackoon1030@gmail.com>
2025-11-30 01:48:53 -0800

    trigger redeploy

--- github-page/src/App.js
+++ github-page/src/App.js
@@ -5,7 +5,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import './styles/styles.css';
-
+//Trigger deploy
 function App() {
   const [activeSection, setActiveSection] = useState('about-me');
 
